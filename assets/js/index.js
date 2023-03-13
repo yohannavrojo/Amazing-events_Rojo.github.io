efdbe9e4db92929075cb1a3d12fa63baa8fbb873
yohannavrojo <yohavale7883@gmail.com>
2023-03-13 09:29:14 -0300

    filter cards checkbox y search

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 
 const cardContainer = document.getElementById('card');
 
+
 for (let i = 0; i < data.events.length; i++) {
   const event = data.events[i];
   const card = document.createElement('div');
-//  card.classList.add("hola")
   card.innerHTML = `
     <div class="card card__colors mx-2" >
       
@@ -18,6 +18,9 @@ for (let i = 0; i < data.events.length; i++) {
             <p class="card-text col">
               ${event.price}$
             </p>
+            <p class="category card-text col">
+            ${event.category}
+          </p>
             <a href="#" class="btn btn-color h-25 ">More </a>
           </div>
         </div>
@@ -28,3 +31,55 @@ for (let i = 0; i < data.events.length; i++) {
   cardContainer.appendChild(card);
 }
 
+
+
+// Obtener todos los checkbox
+const checkboxes = document.querySelectorAll('.filtro input[type="checkbox"]');
+
+// Obtener todas las cartas
+const cards = document.querySelectorAll('.card');
+
+// Agregar un EventListener a cada checkbox
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('change', (event) => {
+    // Obtener los valores de los checkboxes seleccionados
+    const checkedValues = Array.from(checkboxes)
+      .filter((checkbox) => checkbox.checked)
+      .map((checkbox) => checkbox.value);
+  console.log(checkedValues);
+    // Recorrer todas las cartas y mostrar/ocultar según los filtros seleccionados
+    cards.forEach((card) => {
+      const cardCategory = card.querySelector('.category').textContent;
+   console.log(card);
+      if (checkedValues.length > 0 && !checkedValues.includes(cardCategory)) {
+        card.style.display = 'none'; 
+     
+      } else {
+        card.style.display= 'block';
+
+      }
+    });
+  });
+
+});
+
+// filtro search 
+const search = document.getElementById('search');
+// Agregar un event listener al campo de entrada de búsqueda
+search.addEventListener('input', (event) => {
+  // Obtener el texto ingresado en el campo de entrada de búsqueda
+  const searchText = event.target.value.toLowerCase().trim();
+
+  // Recorrer todas las cartas y mostrar/ocultar según el texto ingresado
+  cards.forEach((card) => {
+    const cardName = card.querySelector('h5').textContent.toLowerCase();
+
+    if (cardName.includes(searchText)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+
+  cards.style.flexWrap = 'wrap';
+});
--- assets/js/upComing.js
+++ assets/js/upComing.js
@@ -37,3 +37,54 @@ for (let i = 0; i < data.events.length; i++) {
   }
 
 }
+
+// filtro checkbox 
+// Obtener todos los checkbox
+const checkboxes = document.querySelectorAll('.filtro input[type="checkbox"]');
+
+// Obtener todas las cartas
+const cards = document.querySelectorAll('.card');
+
+// Agregar un EventListener a cada checkbox
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('change', (event) => {
+    // Obtener los valores de los checkboxes seleccionados
+    const checkedValues = Array.from(checkboxes)
+      .filter((checkbox) => checkbox.checked)
+      .map((checkbox) => checkbox.value);
+  console.log(checkedValues);
+    // Recorrer todas las cartas y mostrar/ocultar según los filtros seleccionados
+    cards.forEach((card) => {
+      const cardCategory = card.querySelector('.category').textContent;
+   console.log(card);
+      if (checkedValues.length > 0 && !checkedValues.includes(cardCategory)) {
+        card.style.display = 'none'; 
+     
+      } else {
+        card.style.display= 'block';
+
+      }
+    });
+  });
+});
+
+
+
+// filtro search 
+const search = document.getElementById('search');
+// Agregar un event listener al campo de entrada de búsqueda
+search.addEventListener('input', (event) => {
+  // Obtener el texto ingresado en el campo de entrada de búsqueda
+  const searchText = event.target.value.toLowerCase().trim();
+
+  // Recorrer todas las cartas y mostrar/ocultar según el texto ingresado
+  cards.forEach((card) => {
+    const cardName = card.querySelector('h5').textContent.toLowerCase();
+
+    if (cardName.includes(searchText)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+});
